ed07e3df0864cd134e4caf1b3f5397ed6361253c
OlgaMatveeva-04 <64786805+OlgaMatveeva-04@users.noreply.github.com>
2020-05-04 16:52:46 +0300

    Update class Main.java

--- class Main.java
+++ class Main.java
@@ -1,5 +1,14 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+ for(int i = 2; i <= 100; i ++){
+            boolean isPrime = true;
+
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime){
+                System.out.println(i);
+            }
+        }
